ec0510e0320b616ba2dbf8b419b75f824d87dd00
HERVE NGUYEN <83554610+TsunomakiWatamelon@users.noreply.github.com>
2023-12-02 01:09:17 +0100

    Partie 1 : Predicat autoref

--- solveur.pl
+++ solveur.pl
@@ -2,10 +2,11 @@
 % M1 Informatique SU
 % Robin Soares & Herve Nguyen
 
-programme() :- load_files('tabox.pl').
+programme() :- load_files('tabox.pl'), load_files('aux.pl').
 
 % ##### Partie 1 #####
 
+% On cree liste qui representera la TBox et les deux listes qui represente la ABox
 setof((Concept, Definition), equiv(Concept, Definition), TBox).
 setof((Instance, Concept), inst(Instance,Concept), ABoxC).
 setof((Instance1, Instance2, Role), instR(Instance1, Instance2, Role), ABoxR).
@@ -36,3 +37,22 @@ concept(some(Role, Concept)) :- isR(Role), concept(ConceptY).
 concept(all(Role, Concept)) :- isR(Role), concept(ConceptY).
 
 % Prédicats
+
+autoref(Concept) :- equiv(Concept, Expression), conceptAutoref(Concept, Expression).
+
+conceptAutoref(Concept, Concept) :- 
+    write('Erreur : Un concept auto-référent a été detecté'), 
+    fail.
+
+conceptAutoref(Concept, some(Expression)) :- conceptAutoref(Concept, Expression).
+conceptAutoref(Concept, not(Expression)) :- conceptAutoref(Concept, Expression).
+conceptAutoref(Concept, and(Expression1, Expression2)) :- 
+    conceptAutoref(Concept, Expression1);
+    conceptAutoref(Concept, Expression2).
+conceptAutoref(Concept, or(Expression1, Expression2)) :- 
+    conceptAutoref(Concept, Expression1);
+    conceptAutoref(Concept, Expression2).
+conceptAutoref(Concept, some(Role, Expression)) :- conceptAutoref(Concept, Expression).
+conceptAutoref(Concept, all(Role, Expression)) :- conceptAutoref(Concept, Expression).
+
+% traitement_Tbox 
