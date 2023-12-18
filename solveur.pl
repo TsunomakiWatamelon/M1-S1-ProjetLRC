df473124a08ad9045a083ddffb4788eff679b4d9
HERVE NGUYEN <83554610+TsunomakiWatamelon@users.noreply.github.com>
2023-12-18 02:43:39 +0100

    partie 3 : ensemble predicats ecrits, non testés

--- solveur.pl
+++ solveur.pl
@@ -2,12 +2,11 @@
 % M1 Informatique SU
 % Robin Soares & Herve Nguyen
 
-compteur(1).
-
 programme() :- load_files('tabox.pl'),
                load_files('aux.pl'),
                load_files('partie1.pl'),
                load_files('partie2.pl'),
+               load_files('partie3.pl'),
                premiere_etape(Tbox,Abi,Abr),
                deuxieme_etape(Abi,Abi1,TBox).
 
