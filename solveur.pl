bcfe4cad941bbb4228761922e133ad2a6dbcb95f
HERVE NGUYEN <83554610+TsunomakiWatamelon@users.noreply.github.com>
2023-12-18 04:19:27 +0100

    Partie 3, liaison solveur.pl vers partie 3, non fonctionnel

--- solveur.pl
+++ solveur.pl
@@ -8,5 +8,6 @@ programme() :- load_files('tabox.pl'),
                load_files('partie2.pl'),
                load_files('partie3.pl'),
                premiere_etape(Tbox,Abi,Abr),
-               deuxieme_etape(Abi,Abi1,TBox).
+               deuxieme_etape(Abi,Abi1,TBox),
+               troisieme_etape(Abi1,Abr).
 
